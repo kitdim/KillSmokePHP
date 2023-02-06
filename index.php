ac54c683268ca442ad8955211c786ef50bd2ca8a
kitdim <kitaykin9808@gmail.com>
2023-02-06 22:19:33 +0300

    добавил стили

--- index.php
+++ index.php
@@ -1,21 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-<link rel="stylesheet" href="css/style.css" type="text/css"/>
+<link rel="stylesheet" href="css/style.css" type="text/css" />
+
 <head>
-<title>Kill Smoke</title>
-<meta charset="utf-8" />
+    <title>Kill Smoke</title>
+    <meta charset="utf-8" />
 </head>
+
 <body>
-<h3>Форма ввода данных</h3>
-<form action="func.php" method="POST">
-    <p>Ты курил?</p>
-    <div class="radio-container">
-        <input class="radio-input" id="answer" type="radio" name="answer" />
-        <label class="radio" for="answer">+</label> 
-        <input class="radio-input" id="answer" type="radio" name="answer" />
-        <label class="radio" for="answer">-</label>
-    </div>
-    <input type="submit" value="Отправить">
-</form>
+    <h3 class="text">Форма ввода данных</h3>
+    <form action="func.php" method="POST">
+        <p class="text">Ты курил?</p>
+        <div class="radio-container">
+            <input class="radio-input" id="answer" type="radio" name="answer" />
+            <label class="radio" for="answer">+</label>
+            <input class="radio-input" id="answer" type="radio" name="answer" />
+            <label class="radio" for="answer">-</label>
+        </div>
+        <input class="buttonSend" type="submit" value="Отправить">
+    </form>
 </body>
+
 </html>
